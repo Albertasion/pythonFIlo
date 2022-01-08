b84e1456e7f7a90c9047fec194967fc25f546e0e
albertasion <berekas1986@gmail.com>
2022-01-08 20:44:00 +0200

    version 0.0.1

--- FILO/create_profile.py
+++ FILO/create_profile.py
@@ -4,6 +4,91 @@ from aiogram import types, Dispatcher
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from loader import dp
 from aiogram import Bot
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.utils.callback_data import CallbackData
+from aiogram.utils.exceptions import MessageNotModified
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from aiogram.utils.callback_data import CallbackData
+# callback_numbers = CallbackData("fabnum", "action")
+#
+# def get_keyboard_fab():
+#     buttons = [
+#         types.InlineKeyboardButton(text="-1", callback_data=callback_numbers.new(action="decr")),
+#         types.InlineKeyboardButton(text="-1", callback_data=callback_numbers.new(action="icr")),
+#         types.InlineKeyboardButton(text="Подтвердить", callback_data=callback_numbers.new(action="finish"))
+#     ]
+#     keyboard = types.InlineKeyboardMarkup(row_width=2)
+#     keyboard.add(*buttons)
+#     return keyboard
+#
+# async def update_num_text_fab(message: types.Message, new_value:int):
+#     with suppress(MessageNotModified):
+#         await message.edit_text(f'Ukagite chislo:{new_value}', reply_markup=get_keyboard_fab())
+#
+# @dp.message_handler(commands="number_fab")
+# async def cmd_numbers(message: types.Message):
+#     user_data[message.from_user.id] = 0
+#     await message.answer("Ukagite 0", reply_markup=get_keyboard_fab())
+#
+# @dp.callback_query_handler(callback_numbers.filter(action=["incr", "decr"]))
+# async def callbacks_num_change_fab(call:types.CallbackQuery, callback_data:dict):
+#     user_value = user_data.get(call.from_user.id, 0)
+#     action=callback_data["action"]
+#     if action=="incr":
+#         user_data[call.from_user.id]=user_value+1
+#         await update_num_text_fab(call.message, user_value+1)
+#     elif action=="decr":
+#         user_data[call.from_user.id]=user_value-1
+#         await update_num_text_fab(call.message, user_value-1)
+#     await call.answer()
+#
+# @dp.callback_query_handler(callback_numbers.filter(action=["finish"]))
+# async def callbacks_num_finish_fab(call: types.CallbackQuery):
+#     user_value = user_data.get(call.from_user.id, 0)
+#     await call.message.edit_text(f"Итого: {user_value}")
+#     await call.answer()
+
+
 
 
 
@@ -44,6 +129,7 @@ async def msg_load_photo(message: types.Message):
 async def load_profile_photo(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['photo'] = message.photo[-1].file_id
+        print(data['photo'])
         await FSMProfile.next()
         await message.answer("Введите ваше имя")
         # await dp.bot.send_message(679511058, "Bot startoval")
--- FILO/loader.py
+++ FILO/loader.py
@@ -1,9 +1,120 @@
+from unittest.mock import call
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.types import message
+from oauthlib.uri_validate import query
+
 from config import *
+from contextlib import suppress
+
+from aiogram import types
+from aiogram.utils.exceptions import (MessageToEditNotFound, MessageCantBeEdited, MessageCantBeDeleted,
+    MessageToDeleteNotFound)
 
 
 #инициализация бота
 bot = Bot(token=API_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.utils.callback_data import CallbackData
+from aiogram.utils.exceptions import MessageNotModified
+
+logging.basicConfig(level=logging.INFO)
+
+dp.middleware.setup(LoggingMiddleware())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+vote_cb = CallbackData('vote', 'action', 'amount')  # post:<action>:<amount>
+
+
+def get_keyboard(amount):
+    return types.InlineKeyboardMarkup().add(
+        types.InlineKeyboardButton('👍 Нравится', callback_data=vote_cb.new(action='up', amount=amount)),
+        types.InlineKeyboardButton('👎 Пропустить', callback_data=vote_cb.new(action='down', amount=amount)),
+        types.InlineKeyboardButton('ℹ️ Подробнее', callback_data=vote_cb.new(action='detail_profile', amount=amount)),
+        types.InlineKeyboardButton('📧 Написать', callback_data=vote_cb.new(action='write_letter', amount=amount)),
+        types.InlineKeyboardButton('🗑️ Жалоба', callback_data=vote_cb.new(action='spam_profile', amount=amount))
+    )
+async def delete_message(message: types.Message, sleep_time: int = 0):
+    await asyncio.sleep(sleep_time)
+    with suppress(MessageCantBeDeleted, MessageToDeleteNotFound):
+        await message.delete()
+
+
+@dp.message_handler(commands='search')
+async def cmd_start(message: types.Message):
+    await message.answer_photo('AgACAgIAAxkBAAIGBmHZjiqxpsaWLsVI0I8GNmmDMHJ7AAJEuTEb2p7ISgx2CrqzENccAQADAgADeAADIwQ', caption='<b>Petr</b>. 25 лет. <s>Киев.</s> <i>Украина</i>. ИТ специалист по безопасности. Интересы:<pre>Джаз</pre>', reply_markup=get_keyboard(0))
+
+
+
+@dp.callback_query_handler(vote_cb.filter(action='up'))
+async def vote_up_cb_handler(call: types.CallbackQuery, callback_data: dict):
+    logging.info(callback_data)
+    amount = int(callback_data['amount'])
+    await call.message.answer(amount)
+    await call.answer("Вы проголосовали за ❤️")
+    await call.message.answer("Ждите ответ")
+    amount+=1
+    await bot.edit_message_text(f'You voted down! Now you have {amount} votes.',
+                                query.from_user.id,
+                                query.message.message_id,
+                                reply_markup=get_keyboard(amount))
+
+
+
+
+
+
+
+
+@dp.callback_query_handler(vote_cb.filter(action='down'))
+async def vote_down_cb_handler(query: types.CallbackQuery, callback_data: dict):
+    amount = int(callback_data['amount'])
+    amount -= 1
+    await bot.edit_message_text(f'You voted down! Now you have {amount} votes.',
+                                query.from_user.id,
+                                query.message.message_id,
+                                reply_markup=get_keyboard(amount))
+@dp.callback_query_handler(vote_cb.filter(action='detail_profile'))
+async def vote_down_cb_handler(query: types.CallbackQuery, callback_data: dict):
+    amount = int(callback_data['amount'])
+    await bot.edit_message_text(f'You voted down! Now you have {amount} votes.',
+                                query.from_user.id,
+                                query.message.message_id,
+                                reply_markup=get_keyboard(amount))
+
+
+
+@dp.errors_handler(exception=MessageNotModified)  # for skipping this exception
+async def message_not_modified_handler(update, error):
+    return True
